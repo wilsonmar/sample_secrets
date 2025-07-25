7d88136ef2304224a34b432d6bafe68536ad97e6
Wilson Mar <wilsonmar+github@gmail.com>
2025-07-24 18:31:54 -0600

    v002 + add gitleaks allow :bucket_s3.py

--- bucket_s3.py
+++ bucket_s3.py
@@ -1,4 +1,13 @@
-# Save data to an AWS bucket
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MPL-2.0
+
+__commit_date__ = "2025-07-24"
+__commit_msg__ = "v002 + add gitleaks allow :bucket_s3.py"
+__repository__ = "https://github.com/wilsonmar/sample-secrets/blob/main/sample-secrets.py"
+# Adapted from https://github.com/GitGuardian/sample-secrets
+__status__ = "Intentional secrets for secret scanners gitleaks and GitGuardian to find secrets in code."
+# WARNING: GitHub scans would disallow git commit of this file.
 
 from typing import Dict
 
@@ -15,6 +24,10 @@ def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
 
 MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
+
+# Mark a secret value to ignore by the scanner:
+# https://help.aikido.dev/code-scanning/scanning-practices/ignoring-secrets-via-code-comments
+a = "live_cdrBarsVQi4EGFRJi" # gitleaks:allow
 
 def pull_data_from_mongo(query: Dict):
     return pymongo.connect(MONGO_URI).fetch(query)
